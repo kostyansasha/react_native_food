582728e2f48383861877605b4db68202d1786fca
oleksandr.kostian <kostyansasha@ukr.net>
2023-07-04 13:55:26 +0200

    init, search bar, packages

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import SearchScreen from './src/screens/SearchScreen';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const navigator = createStackNavigator(
+  {
+    Search: SearchScreen,
   },
-});
+  {
+    initialRouteName: 'Search',
+    defaultNavigationOptions: {
+      title: 'Business Search',
+    },
+  }
+);
+
+export default createAppContainer(navigator);
+                                                      
+
+
+//cfxUswlGGggMK4DKIst7wA
+//API Key    oP-fc5eLI8T34OYQ7AfCS6I_ATk2GV4Ea8l8TfSPLGrMjg77fqrczGNenOk6jsT1nWq4eoCnJbAVcsnd0bbnnRS4gJA_TQp5AyZ3TxKZy93pZ-428xH8VnTop-ujZHYx
